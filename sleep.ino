2553fd7822cb3d219e759f829269eec5442717b9
Alan Grier <griera@oregonstate.edu>
2025-04-19 20:39:38 -0700

    Pullup on LIN pin during sleep, so that wakeup will work

--- sleep.ino
+++ sleep.ino
@@ -14,10 +14,18 @@ void goToSleep() {
   delay(10);
   tft.writecommand(0x10);
   delay(10);
+
   digitalWrite(XIAO_BL, LOW);
   gpio_hold_en((gpio_num_t)XIAO_BL);
+
+  detachInterrupt(VEHICLE_LIN);
+  gpio_pulldown_dis((gpio_num_t)VEHICLE_LIN);
+  gpio_pullup_en((gpio_num_t)VEHICLE_LIN);
+  gpio_hold_en((gpio_num_t)VEHICLE_LIN);
+
   rtc_gpio_pullup_dis((gpio_num_t)WAKEUP_PIN);
   rtc_gpio_pulldown_en((gpio_num_t)WAKEUP_PIN);
+
   gpio_deep_sleep_hold_en();
   esp_sleep_enable_ext0_wakeup((gpio_num_t)WAKEUP_PIN, 1);
   esp_deep_sleep_start();
@@ -26,6 +34,10 @@ void goToSleep() {
 void wakeupCleanup() {
   gpio_deep_sleep_hold_dis();
   gpio_hold_dis((gpio_num_t)XIAO_BL);
+
+  gpio_hold_dis((gpio_num_t)VEHICLE_LIN);
+  gpio_pullup_dis((gpio_num_t)VEHICLE_LIN);
+
   rtc_gpio_pulldown_dis((gpio_num_t)WAKEUP_PIN);
   rtc_gpio_deinit((gpio_num_t)WAKEUP_PIN);
 }
